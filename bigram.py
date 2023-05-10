d79d6037f6701d27c4f9c9ddb2f9ec91660ea281
AllenCheung0213 <alcheung0213@gmail.com>
2023-05-09 22:53:55 -0400

    adding transformer block

--- bigram.py
+++ bigram.py
@@ -6,12 +6,14 @@ from torch.nn import functional as F
 batch_size = 32 
 block_size = 8
 max_iters = 3000
-eval_interval = 300
-learning_rate = 1e-2
+eval_interval = 500
+learning_rate = 1e-3
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 eval_iters = 200
-n_embd = 32
-# ------------
+n_embd = 384
+n_head = 6
+n_layer = 6
+dropout = 0.2
 
 torch.manual_seed(1337)
 
@@ -56,23 +58,58 @@ def estimate_loss():
 
 class Head(nn.Module):
   def __init__(self, head_size):
-    super.__init__()
+    super().__init__()
     self.key = nn.Linear(n_embd, head_size, bias = False)
     self.query = nn.Linear(n_embd, head_size, bias = False)
     self.value = nn.Linear(n_embd, head_size, bias = False)
     self.register_buffer('tril', torch.tril(torch.ones(block_size, block_size)))
+
+    self.dropout = nn.Dropout(dropout)
     
   def forward(self, x):
     B, T, C = x.shape
     k = self.key(x)
     q = self.query(x)
-    wei = q @ k.transpose(-2, -1) * C ** -0.5
+    wei = q @ k.transpose(-2, -1) * k.shape[-1] ** -0.5
     wei = wei.masked_fill(self.tril[:T, :T] == 0, float('-inf'))
-    wei = F.softmax(wei, dim = -1)
+    wei = F.softmax(wei, dim = -1) 
+    wei = self.dropout(wei)
     v = self.value(x)
-    out = wei @ v
+    out = wei @ v 
     return out
+  
+class MultiHeadAttention(nn.Module):
+  def __init__(self, num_heads, head_size):
+    super().__init__()
+    self.heads = nn.ModuleList([Head(head_size) for _ in range(num_heads)])
+        
 
+  def forward(self, x):
+    out = torch.cat([h(x) for h in self.heads], dim = -1)
+    return out
+      
+class FeedForward(nn.Module):
+  def __init__(self, n_embd):
+    super().__init__()
+    self.net = nn.Sequential(
+      nn.Linear(n_embd, n_embd),
+      nn.ReLU(),
+    )
+
+  def forward(self, x):
+    return self.net(x)
+  
+class Block(nn.Module):
+  def __init__(self, n_embd, n_head):
+    super().__init__()
+    head_size = n_embd // n_head
+    self.sa = MultiHeadAttention(n_head, head_size)
+    self.ffwd = FeedForward(n_embd)
+  
+  def forward(self, x):
+    x = self.sa(x)
+    x = self.ffwd(x)
+    return x
 
 class BigramLanguageModel(nn.Module):
 
@@ -80,7 +117,8 @@ class BigramLanguageModel(nn.Module):
     super().__init__()
     self.token_embedding_table = nn.Embedding(vocab_size, n_embd)
     self.position_embedding_table = nn.Embedding(block_size, n_embd)
-    self.sa_head = Head(n_embd)
+    self.sa_heads = MultiHeadAttention(4, n_embd//4)
+    self.ffwd = FeedForward(n_embd)
     self.lm_head = nn.Linear(n_embd, vocab_size)
 
   def forward(self, idx, targets=None):
@@ -89,7 +127,8 @@ class BigramLanguageModel(nn.Module):
     tok_emb = self.token_embedding_table(idx) 
     pos_emb = self.position_embedding_table(torch.arange(T, device = device))
     x = tok_emb + pos_emb
-    x = self.sa_head(x)
+    x = self.sa_heads(x)
+    x = self.ffwd(x)
     logits = self.lm_head(x)
 
     if targets is None:
@@ -103,7 +142,8 @@ class BigramLanguageModel(nn.Module):
 
   def generate(self, idx, max_new_tokens):
     for _ in range(max_new_tokens):
-      logits, loss = self(idx)
+      idx_cond = idx[:, -block_size:]
+      logits, loss = self(idx_cond)
       logits = logits[:, -1, :]
       probs = F.softmax(logits, dim = -1) 
       idx_next = torch.multinomial(probs, num_samples=1)
@@ -117,17 +157,17 @@ optimizer = torch.optim.AdamW(model.parameters(), lr = learning_rate)
 
 for iter in range(max_iters):
 
-  if iter % eval_interval == 0:
+  if iter % eval_interval == 0 or iter == max_iters - 1:
     losses = estimate_loss()
     print(f"step {iter}: train loss {losses['train']:.4f}, val loss {losses['val']:.4f}")
 
   xb, yb = get_batch('train')
     
   logits, loss = model(xb, yb)
-  optimizer.zero_grad(set_to_none=True)
+  optimizer.zero_grad(set_to_none = True)
   loss.backward()
   optimizer.step()
 
 # generate from the model
 context = torch.zeros((1, 1), dtype=torch.long, device=device)
-print(decode(m.generate(context, max_new_tokens=500)[0].tolist()))
+print(decode(m.generate(context, max_new_tokens = 500)[0].tolist()))
